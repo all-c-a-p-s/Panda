03254f68c5f7b2674e011f426815deaeeefb398e
Sebastiano Rebonato-Scott <141144328+all-c-a-p-s@users.noreply.github.com>
2024-03-03 15:04:05 +0000

    generate magics

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,8 @@ pub mod helper;
 pub mod magic;
 pub mod rng;
 
-use crate::helper::*;
 use crate::magic::*;
-use crate::rng::*;
 
 fn main() {
-    let blockers: u64 = 0;
-    let bit = mask_queen_attacks(square("d4"), blockers);
-    let occupancy = set_occupancy(50000, count(bit), bit);
-    print_bitboard(occupancy);
-    println!();
-    for _ in 0..5 {
-        println!("{}", generator_random_number())
-    }
+    init_magics();
 }
